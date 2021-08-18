b73da3590fd4cb5101cf7c8470b63ee37f84840f
John Long <johnlonggately@gmail.com>
2021-08-18 10:45:04 +0000

    added components folder and FunctionalGreeting file and modified App.js

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function FunctionalGreeting() {
+//     return <h1>Hello from React!</h1>
+// }
+
+const FunctionalGreeting = () => <h1>Hello from React!</h1>;
+
+export default FunctionalGreeting;
