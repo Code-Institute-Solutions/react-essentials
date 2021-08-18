71a21ec0243d54e0fa3c1f3f6699faeadc838d65
John Long <johnlonggately@gmail.com>
2021-08-18 13:45:48 +0000

    Updating the State, Part 1

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -10,12 +10,18 @@ class StatefulGreeting extends React.Component{
         };
     }
 
+    handleClick() {
+        this.setState({
+            introduction: "Goodbye!"
+        });
+        console.log(this.state.introduction);
+    }
 
     render() {
         return (
             <div>
                 <h1>{this.state.introduction} {this.props.greeting}</h1>
-                <button>{this.state.buttonText}</button>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
             </div>
         )
     }
